b1ce93827a5b4d6c1f8481013a74fbe62766e0b7
smit patel <smit.patel112001@gmail.com>
2025-06-04 14:21:04 -0500

    Update db.js

--- backendProject/src/db.js
+++ backendProject/src/db.js
@@ -5,8 +5,11 @@ const pool = new Pool({
   user: "postgres",
   host: "database-2.cn0geogwil7k.us-east-2.rds.amazonaws.com",
   database: "library_db",
-  password: "zxcvbnm1",
+  password: "MliGSEbLpRtIY4IWBSyC",
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false, // For development only. 
+  },
 });
 
 //export default pool;
